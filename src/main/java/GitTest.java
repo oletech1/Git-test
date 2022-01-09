78c5d34daa5599db7d8fc67b7e0c9aaa9db1ca56
WangChao <114347680@qq.com>
2022-01-09 21:59:34 +0800

    master test commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("1");
         System.out.println("2");
         System.out.println("2-merge");
+        System.out.println("master test");
     }
 }
