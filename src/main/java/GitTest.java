28ea4f5559b5a3477554ecdb7aba5223bac1c8d2
WangChao <114347680@qq.com>
2022-01-09 22:30:20 +0800

    push commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("2-merge");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push commit");
     }
 }
