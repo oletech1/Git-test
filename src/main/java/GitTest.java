d758a94a5815b7749d09d6c7a64a76dd59717a49
WangChao <114347680@qq.com>
2022-01-09 21:40:34 +0800

    2 commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("1");
+        System.out.println("2");
     }
 }
